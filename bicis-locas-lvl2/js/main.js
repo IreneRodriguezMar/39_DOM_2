241463b47c8c80d62e9a3053c5bf355e92744852
IreneRodriguezMar <irene.rodriguezmar@gmail.com>
2016-10-19 15:01:52 +0000

    cambios en los span

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -1,19 +1,52 @@
 function validateForm(){
+	//llamas a la funcion desaparecer aqui para que no los ponga de principio
+	
+	var spans = document.getElementsByClassName("falla");
+	while(spans.length>0 ){
+		spans[0].parentElement.removeChild(spans[0]);
+	}
+	
 	/* Escribe tú código aquí */
 	var name = document.getElementById("name").value;
 	var apellido = document.getElementById("lastname").value;
 	var correo = document.getElementById("input-email").value;
 	var contraseña = document.getElementById("input-password").value;
-	var spanNombre = document.getElementById("mySpan").style.display;
-	var spanContraseña = document.getElementById("passw").style.display;
+	var opcion = document.getElementById("opciones").selectedIndex;
 	
-	if(name === "" || apellido === "" || correo === ""){
-		return document.getElementById("mySpan").style.display = "inline";
+	if(name === "" ){
+		var validacionCorreo = document.createElement('span');
+		validacionCorreo.setAttribute("class","falla");
+		validacionCorreo.appendChild(document.createTextNode("Debe ingresar su nombre")); 
+		document.getElementById("name").parentElement.appendChild(validacionCorreo);
 	}
-	else if(contraseña.length < 6){
-		return document.getElementById("passw").style.display = "inline";
+	if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(correo))){
+		var validacionCorreo = document.createElement('span');
+		validacionCorreo.setAttribute("class","falla");
+		validacionCorreo.appendChild(document.createTextNode("Verifique su e-mail")); 
+		document.getElementById("input-email").parentElement.appendChild(validacionCorreo);
 	}
-	else if(contraseña === "password" || "123456" || "098754"){
-		alert("Contraseña no válida, escriba una diferente")
+	
+	if(contraseña === "password" || contraseña === "123456" || contraseña === "098754"){
+		var validacionContraseña = document.createElement('span');
+		validacionContraseña.setAttribute("class","falla");
+		validacionContraseña.appendChild(document.createTextNode("Contraseña no valida, escriba una diferente")); 
+		document.getElementById("input-password").parentElement.appendChild(validacionContraseña);
 	}
+	
+	if(contraseña.length < 6 ){
+		var validacionLongitud = document.createElement('span');
+		validacionLongitud.setAttribute("class","falla");
+		validacionLongitud.appendChild(document.createTextNode("La contraseña debe tener al menos 6 carácteres")); 
+		document.getElementById("input-password").parentElement.appendChild(validacionLongitud);
+	}	
+	
+	if(opcion == 0){
+		var validacionOpcion = document.createElement('span');
+		validacionOpcion.setAttribute("class","falla");
+		validacionOpcion.appendChild(document.createTextNode("Debes seleccionar al menos un tipo de bici")); 
+		document.getElementById("opciones").parentElement.appendChild(validacionOpcion);
+	}
+
+//con esto borra todos los spans
+
 }
